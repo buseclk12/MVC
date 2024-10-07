5e59d54f63c1c89b6c9d841df33fc1a2352108f1
yalinmocan <yalinmocan@gmail.com>
2024-10-07 20:54:49 +0300

    Add project files.

---
+++ BLL/Models/StudentModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MVC.BLL.Models
+{
+    public class StudentModel
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+                
+        public string LastName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+    }
+}
